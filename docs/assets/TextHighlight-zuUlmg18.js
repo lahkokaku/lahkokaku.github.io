09b504cea738028074ce9d33f6e1798bd114b02d
Jose Stephen <jose.satrya@gmail.com>
2025-03-22 21:22:06 +0700

    deploy: 2025/03/22-21:22

---
+++ docs/assets/TextHighlight-zuUlmg18.js
@@ -0,0 +1,1 @@
+import{r as e,j as a}from"./index-uQDukMyP.js";import{c as n}from"./clsx-B-dksMZM.js";const x=({children:t,className:s})=>{const r=e.useRef(null);return a.jsx("span",{ref:r,className:n("bg-gradient-light bg-clip-text","transition text-transparent",s),children:t})};export{x as default};
